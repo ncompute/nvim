22b545c7ee42be01ae64cec223602d8902596700
scott <scott@clarkcode.com>
2024-12-21 00:46:37 +0000

    ToykoNight Theme

--- lua/plugins/theme.lua
+++ lua/plugins/theme.lua
@@ -1,8 +1,9 @@
 return {
-  "catppuccin/nvim",
-  name = "catppuccin",
-  priority = 1000,
-  config = function()
-    vim.cmd.colorscheme "catppuccin"
-  end
+	"folke/tokyonight.nvim",
+	lazy = false,
+	priority = 1000,
+	opts = {},
+	config = function()
+		vim.cmd.colorscheme("tokyonight-night")
+	end,
 }
