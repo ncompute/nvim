ea85150162be386a0c238daca22a1447bcb12ae9
scott <scott@clarkcode.com>
2024-12-22 17:02:29 +0000

    Improved file navigation

---
+++ Readme.md
@@ -0,0 +1,13 @@
+# NVIM
+
+#### Mappings
+```
+leader leader : search files
+leader fg     : search git files
+leader fs     : live grep
+leader fb     : search buffers
+leader e      : open file explorer
+leader n      : next buffer
+leader p      : previous buffer
+leader x      : close buffer
+```
--- lua/core/config.lua
+++ lua/core/config.lua
@@ -6,3 +6,9 @@ vim.g.mapleader = " "
 vim.g.loaded_netrw = 1
 vim.g.loaded_netrwPlugin = 1
 vim.wo.number = true
+vim.opt.termguicolors = true
+
+-- Keymaps
+vim.keymap.set("n", "<leader>n", ":bnext<CR>", {})
+vim.keymap.set("n", "<leader>p", ":bprevious<CR>", {})
+vim.keymap.set("n", "<leader>x", ":bd<CR>", {})
